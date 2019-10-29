8111d40a4a3d6572f3fe7938d7fd2a20fc3939c0
Rajnish Kr Singh <rajnishsinghrs344@gmail.com>
2019-10-29 17:19:51 +0530

    Updates

---
+++ precache-manifest.b0c6e3a21bfc96f433146a4b0446cea0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e7f242b223e12cf5406a48b41d23bc",
+    "url": "/NAPOD/index.html"
+  },
+  {
+    "revision": "964ecdd7b210c413af38",
+    "url": "/NAPOD/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "46813d7189c5325ce0ef",
+    "url": "/NAPOD/static/css/main.bc54a38f.chunk.css"
+  },
+  {
+    "revision": "964ecdd7b210c413af38",
+    "url": "/NAPOD/static/js/2.ee158c2b.chunk.js"
+  },
+  {
+    "revision": "46813d7189c5325ce0ef",
+    "url": "/NAPOD/static/js/main.bd513b1e.chunk.js"
+  },
+  {
+    "revision": "94d12dadb8b7fb814301",
+    "url": "/NAPOD/static/js/runtime-main.b848e7ea.js"
+  }
+]);
